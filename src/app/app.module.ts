2545dbc45e09233371397c602a6819b90485636a
brunoBMonteiro <brunobilheri@hotmail.com>
2021-08-29 18:08:40 -0300

    Creating Components and Course List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';"@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+export class CourseListComponent implements OnInit {
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+        {
+            id: 1,
+            name: 'Curso Angular',
+            imageUrl: '',
+            price: 99.99,
+            code: 'xps-1395',
+            duration: 100,
+            rating: 4.4,
+            releaseDate: 'Outubro, 8, 2021',
+        },
+        {
+            id: 2,
+            name: 'Curso TypeScript',
+            imageUrl: '',
+            price: 99.99,
+            code: 'asdsd-1395',
+            duration: 122,
+            rating: 4.7,
+            releaseDate: 'Outubro, 8, 2021',
+        }
+    ]
+    }
+}
