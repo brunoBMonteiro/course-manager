3984a49f9e001d0532a01dc3d34546651a0d56bf
brunoBMonteiro <brunobilheri@hotmail.com>
2021-09-07 19:17:49 -0300

    Handling data binding events

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,35 +1,32 @@
 import { Component, OnInit } from '@angular/core';"@angular/core";
 import { Course } from "./course";
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = [];
+    filteredCourses: Course[] = [];
+    
+    _courses: Course[] = [];
+
+    _filterBy!: string;
+
+    constructor(private courseService: CourseService) {}
 
     ngOnInit(): void {
-        this.courses = [
-        {
-            id: 1,
-            name: 'Angular Forms',
-            imageUrl: './assets/images/forms.png',
-            price: 99.99,
-            code: 'xps-1395',
-            duration: 100,
-            rating: 4.4,
-            releaseDate: 'Outubro, 8, 2021',
-        },
-        {
-            id: 2,
-            name: 'Angular Http',
-            imageUrl: './assets/images/http.png',
-            price: 99.99,
-            code: 'asdsd-1395',
-            duration: 122,
-            rating: 4.7,
-            releaseDate: 'Outubro, 8, 2021',
-        }
-    ]
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter(){
+        return this._filterBy;
     }
 }
